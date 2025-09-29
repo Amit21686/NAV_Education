55c9be883226f7da99d20dcc8c245b3e3bbd23b8
Amit21686 <paamit21@gmail.com>
2025-09-29 09:53:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/service-worker.js")
-      .then(reg => console.log("SW registered:", reg))
-      .catch(err => console.log("SW failed:", err));
+      .then(reg => console.log("Service Worker Registered:", reg))
+      .catch(err => console.log("Service Worker Failed:", err));
   });
 }

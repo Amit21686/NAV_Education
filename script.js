428352cbf008d5df39011fe39d9a6cfe822b6ef3
Amit21686 <paamit21@gmail.com>
2025-11-22 17:44:31 +0530

    Update script.js
    delete

--- script.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/nav-education/service-worker.js")
-      .then(reg => console.log("Service Worker Registered:", reg))
-      .catch(err => console.log("Service Worker Failed:", err));
-  });
-}
